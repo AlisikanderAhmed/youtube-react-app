33ab02745604e0c6390f824e308d615edeacdb84
a256ahme <a256ahme@scs.ryerson.ca>
2018-05-06 18:06:37 -0400

    V2 Commit. Channel Searcher

--- src/App.js
+++ src/App.js
@@ -9,8 +9,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Youtube Search</h1>
+          <p className="App-title-p">Coded Using React & Youtube API</p>
         </header>
+        <h1>Search Youtube Channel Name</h1>
         <Youtube />
       </div>
     );
--- src/Youtube.js
+++ src/Youtube.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
 
-
 const API = 'AIzaSyAxjAvIUVI4pfPv1MF0mMefSqQ2PSy_GuQ'
-const channelId = 'UC_x5XG1OV2P6uZZ5FSM9Ttw'
 const result = 5;
 
-var url = `https://www.googleapis.com/youtube/v3/search?key=${API}&channelId=${channelId}&part=snippet,id&order=date&maxResults=${result}`
+var url = `https://www.googleapis.com/youtube/v3/search?key=${API}&part=snippet,id&order=date&maxResults=${result}&channelId=`
+var channelurl = `https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&key=${API}&forUsername=`
 
 class Youtube extends Component {
 
@@ -13,34 +12,101 @@ class Youtube extends Component {
     super(props);
 
     this.state = {
-      resultyt: []
+      resultyt: [],
+      value: '',
+      channelname: '',
+      channelicon: '',
+      channelid: '',
+      channelcreated: '',
+      description: '',
+      statistics: []
     };
     this.clicked = this.clicked.bind(this);
-}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 
-clicked(){
-  fetch(url)
+  }
+
+  clicked(){
+    url+=this.state.channelid;
+    fetch(url)
     .then((response) => response.json())
     .then((responseJson) => {
       const resultyt = responseJson.items.map(obj => "https://www.youtube.com/embed/"+obj.id.videoId);
       this.setState({resultyt});
     })
+
     .catch((error) => {
       console.error(error);
     });
-}
+  }
 
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+
+    event.preventDefault();
+
+    channelurl += this.state.value;
+
+    fetch(channelurl)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      const channelname = responseJson.items[0].snippet.title;
+      const channelicon = responseJson.items[0].snippet.thumbnails.default.url;
+      const channelcreated = responseJson.items[0].snippet.publishedAt;
+      const statistics = responseJson.items[0].statistics;
+      const description = responseJson.items[0].snippet.description;
+      const channelid = responseJson.items[0].id;
+
+      this.setState({channelid});
+      this.setState({description});
+      this.setState({statistics});
+      this.setState({channelcreated});
+      this.setState({channelname});
+      this.setState({channelicon});
+
+    })
+
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
+  //value={this.state.value}
+  //Smoove7182954
 
   render(){
-    console.log(this.state.resultyt);
+    //console.log(this.state.resultyt);
+    console.log(this.state.channelName);
 
     return(
       <div>
-        <button onClick={this.clicked}>Fetch Videos</button>
+        <form onSubmit={this.handleSubmit} className="form">
+          <label>
+            <input type="text" className="form-input" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <button type="submit" className="form-button">Search</button>
+        </form>
+
+        <h1> Channel Name: {this.state.channelname} </h1>
+        <h2> Date Created: {this.state.channelcreated.substring(0,10)} </h2>
+        <h2> {this.state.description} <hr></hr> </h2>
+
+        <h2> View Count: {this.state.statistics.viewCount} </h2>
+        <h2> Youtuber's Comment Count: {this.state.statistics.commentCount} </h2>
+        <h2> Subscriber Count: {this.state.statistics.subscriberCount} </h2>
+        <h2> Video Count: {this.state.statistics.videoCount} </h2>
+
+        <img src={this.state.channelicon} alt=""></img>
+        <hr></hr>
+        <button onClick={this.clicked}>Fetch Most Recent Videos</button>
         {
           this.state.resultyt.map((link, i) => {
             console.log(link);
-            var frame = <div className="youtube"> <iframe key={i} width="560" height="315" src={link} frameBorder="0" allowFullScreen></iframe> </div>
+            var frame = <div className="youtube"> <iframe key={i} width="560" height="315" src={link} frameBorder="0" allowFullScreen title="Youtube Video"></iframe> </div>
             return frame;
           })
         }
